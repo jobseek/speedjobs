7d0258db21bc6999357de3ce478c870c45a95114
boki <lsb530@naver.com>
2021-04-20 15:17:52 +0900

    시간날리게해서 sorry

--- src/main/java/com/jobseek/speedjobs/domain/tag/RecruitTag.java
+++ src/main/java/com/jobseek/speedjobs/domain/tag/RecruitTag.java
@@ -1,18 +1,23 @@
 package com.jobseek.speedjobs.domain.tag;
 
-import com.jobseek.speedjobs.domain.BaseTimeEntity;
-import com.jobseek.speedjobs.domain.post.Post;
-import com.jobseek.speedjobs.domain.recruit.Recruit;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
 import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
+
+import com.jobseek.speedjobs.domain.BaseTimeEntity;
+import com.jobseek.speedjobs.domain.recruit.Recruit;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
@@ -26,7 +31,7 @@ public class RecruitTag extends BaseTimeEntity {
 	@Column(name = "recruit_tag_id")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = LAZY, cascade = ALL)
 	@JoinColumn(name = "recruit_id")
 	private Recruit recruit;
 
