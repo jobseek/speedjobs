f0111641bb17ff2e1ed1473673d98804d1ca455a
swchae516 <swchae516@naver.com>
2021-04-25 16:11:16 +0900

    feat: 개인정보 수정 기능 추가  - 개인정보 조회에서 데이터 수정할때 가져오는거 기능 추가  - 새로고침 오류 수정

--- front/src/components/pages/ProfileMd.js
+++ front/src/components/pages/ProfileMd.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import {
+  InputText,
   ProfileDiv,
   StyledButton,
   StyledHeaderDiv,
@@ -8,8 +11,66 @@ import {
 } from '../components/Styled';
 import SideMenu from '../components/SideMenu';
 import ProfileModify from '../components/Profile/ProfileModify';
+import ProfileImage from '../components/Profile/ProfileImage';
+import ProfileInputs from '../components/Profile/ProfileInputs';
+import ProfileGender from '../components/Profile/ProfileGender';
+import ProfileTextarea from '../components/Profile/ProfileTextarea';
+import {
+  PROFILE_GET_REQUEST,
+  PROFILE_UPDATE_REQUEST,
+} from '../../reducers/profile';
 
 export default function Profile() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const profile = useSelector((state) => state.profile);
+  const [form, setForm] = useState({
+    name: '',
+    nickname: '',
+    password: '',
+    gender: '',
+    contact: '',
+    bio: '',
+    picture: '',
+    birth: '',
+  });
+
+  const onChangeHandler = useCallback((e) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }, []);
+
+  useEffect(() => {
+    if (profile.profileUpdateDone) {
+      // history.push('/profile');
+      window.location.replace('/profile');
+    }
+  }, [profile, history]);
+
+  const onSubmitHandler = useCallback(
+    (e) => {
+      e.preventDefault();
+      dispatch({ type: PROFILE_UPDATE_REQUEST, data: form, me: user.me.id });
+    },
+    [dispatch, form]
+  );
+
+  useEffect(() => {
+    if (profile.profileGetData) {
+      console.log('테스트', profile.profileGetData);
+      const profileTemp = { ...profile.profileGetData };
+      if (profile.profileGetData.picture === null) {
+        profileTemp.picture =
+          'http://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png';
+      }
+      setForm({ ...profileTemp });
+    }
+  }, [profile.profileGetData]);
+
+  useEffect(() => {
+    if (user.me === null) return;
+    dispatch({ type: PROFILE_GET_REQUEST, data: user.me.id });
+  }, [user.me, dispatch]);
   return (
     <div className="container text-left">
       <StyledHeaderDiv padding style={{ position: 'relative' }}>
@@ -24,8 +85,12 @@ export default function Profile() {
             className={'col-md-3 col-4 text-right'}
             style={{ paddingRight: '0' }}
           >
-            <StyledButton style={{ marginRight: '0' }} wide>
-              개인정보 저장
+            <StyledButton
+              style={{ marginRight: '0' }}
+              wide
+              onClick={(e) => onSubmitHandler(e)}
+            >
+              변경 사항 저장
             </StyledButton>
           </div>
         </StyledHeaderMargin>
@@ -36,7 +101,72 @@ export default function Profile() {
             <SideMenu />
           </StyledLeftLayout>
           <ProfileDiv className={'col-12 col-lg-10'}>
-            <ProfileModify />
+            {/* <ProfileModify />*/}
+            <ProfileImage
+              onChange={(e) => onChangeHandler(e)}
+              value={form.picture}
+            />
+
+            {/* 이름 */}
+            <ProfileInputs name={'이름'} />
+            <InputText
+              onChange={(e) => onChangeHandler(e)}
+              name={'name'}
+              type="text"
+              value={form.name}
+            />
+            {/* 닉네임 */}
+            <ProfileInputs name={'닉네임'} />
+            <InputText
+              onChange={(e) => onChangeHandler(e)}
+              name={'nickname'}
+              type="text"
+              value={form.nickname}
+            />
+            {/* 생년월일 */}
+            <ProfileInputs name={'생년월일'} />
+            <InputText
+              onChange={(e) => onChangeHandler(e)}
+              name={'birth'}
+              type="text"
+              value={form.birth}
+            />
+
+            {/* 비밀번호 */}
+            <ProfileInputs name={'비밀번호'} />
+            <InputText
+              onChange={(e) => onChangeHandler(e)}
+              name={'password'}
+              type="password"
+            />
+
+            {/* <ProfileInputs name={'비밀번호 확인'} />*/}
+            {/* <InputText onChange={(e) => onChangeHandler(e)} type="password" />*/}
+
+            {/* 성별: 남, 여 체크 */}
+            <ProfileInputs name={'성별'} />
+            <ProfileGender
+              onChange={(e) => onChangeHandler(e)}
+              name={'gender'}
+              value={form.gender}
+            />
+
+            {/* 연락처: 집 or 핸드폰 */}
+            <ProfileInputs name={'연락처'} />
+            <InputText
+              onChange={(e) => onChangeHandler(e)}
+              name={'contact'}
+              type="tel"
+              maxLength="13"
+              value={form.contact}
+            />
+            {/* 한 줄 소개 */}
+            <ProfileInputs name={'한 줄 소개'} />
+            <ProfileTextarea
+              onChange={(e) => onChangeHandler(e)}
+              name={'bio'}
+              value={form.bio}
+            />
           </ProfileDiv>
         </div>
       </div>
