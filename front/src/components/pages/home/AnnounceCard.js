08e87eb413cde15aa5828965dfbbc22a630b747e
Yujin Lee <yujinl@gmail.com>
2021-04-28 12:39:52 +0900

    fix: 메인화면 카드 수정하는 중

--- front/src/components/pages/home/AnnounceCard.js
+++ front/src/components/pages/home/AnnounceCard.js
@@ -5,7 +5,8 @@ export default function AnnounceCard() {
   const [list] = useState([
     {
       title: '네이버 공채 모집',
-      subtitle: '2021년 하반기 네이버 공채를 모집합니다.',
+      subtitle:
+        '2021년 하반기 네이버 공채를 모집합니다. 2021년 하반기 네이버 공채를 모집합니다.',
       content: '작가님의 건강한 창작 활동을 응원합니다.',
     },
     {
@@ -24,23 +25,13 @@ export default function AnnounceCard() {
       content: '작가님의 건강한 창작 활동을 응원합니다.',
     },
     {
-      title: '네이버 공채 모집',
+      title: '배달의 민족 공채 모집',
       subtitle: '2021년 하반기 네이버 공채를 모집합니다.',
       content: '작가님의 건강한 창작 활동을 응원합니다.',
     },
     {
       title: '카카오 공채 모집',
       subtitle: '2021년 하반기 카카오 공채를 모집합니다.',
-      content: '작가님의 건강한 창작 활동을 응원합니다.',
-    },
-    {
-      title: '라인 경력직 상시모집',
-      subtitle: '2021년 하반기 라인 경력직을 모집합니다.',
-      content: '작가님의 건강한 창작 활동을 응원합니다.',
-    },
-    {
-      title: '쿠팡 경력직 모집',
-      subtitle: '2021년 하반기 쿠팡 경력직을 모집합니다.',
       content: '작가님의 건강한 창작 활동을 응원합니다.',
     },
   ]);
