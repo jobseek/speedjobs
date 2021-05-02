b50ed5138ef0b29205e3b7617bc09dd2bce49a7d
boki <lsb530@naver.com>
2021-05-03 04:28:43 +0900

    fix: final test fix

--- src/main/java/com/jobseek/speedjobs/utils/QueryDslUtil.java
+++ src/main/java/com/jobseek/speedjobs/utils/QueryDslUtil.java
@@ -13,11 +13,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class QueryDslUtil {
 
+	// 정렬 순서, 객체, 필드명을 입력 받아서 OrderSpecifier로 반환
 	public static OrderSpecifier<?> getSortedColumn(Order order, Path<?> parent, String fieldName) {
 		Path<Object> fieldPath = Expressions.path(Object.class, parent, fieldName);
 		return new OrderSpecifier(order, fieldPath);
 	}
 
+	// 위에서 반환된 객체를 List로 만들어서 Pageable과 함께 정렬된 리스트를 반환
 	public static List<OrderSpecifier> getAllOrderSpecifiers(Pageable pageable, Path<?> parent) {
 
 		List<OrderSpecifier> orders = new ArrayList<>();
